af106331025c7ab32279e904689eae031e88139e
Zeedan Khan <36732475+Zeedan345@users.noreply.github.com>
2025-07-22 20:40:39 -0500

    added environ

--- builtin.c
+++ builtin.c
@@ -12,3 +12,14 @@ int CellEnc(char **args) {
     for (int i; environ[i]; i++) {printf("%s\n", environ[i]);}
     return 0;
 }
+int	cellEnv(char **args)
+{
+	extern char	**environ;
+
+	(void)args;
+	if (!environ)
+		return (1);
+	for (int i = 0; environ[i]; i++)
+		p("%s\n", environ[i]);
+	return (0);
+}
